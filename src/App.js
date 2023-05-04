913b6371289a4bb23b53e803c9011cc82a7f5f93
meilani <lauraho2006@gmail.com>
2023-05-04 17:49:36 -0400

    dark mode switch

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { useState } from 'react';
+import Header from './components/Header';
+
 import './App.scss';
 
+
 function App() {
+  const [theme, setTheme] = useState("dark")
+
+  let appTheme = (theme === "dark") ? "App darkTheme" : "App lightTheme"
+
   return (
-    <div className="App lightTheme">
-      <h1>Test</h1>
+    <div className={appTheme}>
+      <div className="app-container">
+        <Header theme={theme} setTheme={setTheme} />
+      </div>
     </div>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,20 @@
-const Header = () => {
+import { useState } from 'react';
+
+const Header = ({ theme, setTheme }) => {
+    const handleTheme = () => {
+        (theme === "dark") ? setTheme("light") : setTheme("dark")
+    }
+    let btnText = (theme === "dark") ? "Light" : "Dark"
     return (
         <header>
-        
+            <div className="container header-container">
+                <div className="col col-1">
+                    <h1>devfinder</h1>
+                </div>
+                <div className="col col-2">
+                    <button onClick={handleTheme}>{btnText}</button>
+                </div>
+            </div>
         </header>
     )
 }
